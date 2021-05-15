c1d860154df7dc3f29f51bd2569c4251e1e899d6
Hosszu Szilard Jozsef <szili.hosszu@yahoo.com>
2021-05-15 13:51:14 +0200

    finomhangolas

--- src/main/java/controllers/LaunchController.java
+++ src/main/java/controllers/LaunchController.java
@@ -11,9 +11,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 
+@Slf4j
 public class LaunchController {
     @FXML
     private Label error;
@@ -28,13 +30,15 @@ public class LaunchController {
     private Button menu;
 
     @FXML
-    private TextField userName;
+    private TextField userName1;
+    @FXML
+    private TextField userName2;
 
     @FXML
     private Pane howToPlay;
 
     private void error(){
-        error.setText("Please enter a username!");
+        error.setText("Please the a usernames!");
     }
 
     @FXML
@@ -48,16 +52,16 @@ public class LaunchController {
     }
 
     public void start(ActionEvent actionEvent) throws IOException {
-        if (userName.getText().isEmpty()) {
+        if (userName1.getText().isEmpty() || userName2.getText().isEmpty()) {
             error();
         } else {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/Game.fxml"));
             Parent root = fxmlLoader.load();
-            fxmlLoader.<GameController>getController().setUsername(userName.getText());
+            fxmlLoader.<GameController>getController().setUsername(userName1.getText(),userName2.getText());
             Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
-            //log.info("Username is set to {}, loading game scene.", userName.getText());
+            log.info("Username1 is set to {}, Username2 is set to {}, loading game scene.", userName1.getText(), userName2.getText());
         }
 
     }
